3d77b78633d15ee81c7be0656b914512c649175a
vasylec <cozma.vasyle@gmail.com>
2025-02-17 07:15:33 +0200

    new changes

--- Formular Creare Cont/Formular Creare Cont/Form1.cs
+++ Formular Creare Cont/Formular Creare Cont/Form1.cs
@@ -8,18 +8,32 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Formular_Creare_Cont
 {
     public partial class Form1 : MaterialSkin.Controls.MaterialForm
     {
         
+        static List<user> users = new List<user>();
+
+
         public Form1()
         {
             InitializeComponent();
+            upload();
             user.setIndex(0);
         }
 
+        private void upload()
+        {
+            string[] lines = File.ReadAllLines("users.txt");
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ');
+                users.Add(new user(words[0], words[1], words[2], words[3], words[4]));
+            }
+        }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
