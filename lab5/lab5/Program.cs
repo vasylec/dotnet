466aae0bbf66392ed128cdf69ea7463099dbd037
vasylec <cozma.vasyle@gmail.com>
2025-02-19 12:52:13 +0200

    changes

--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -20,11 +20,11 @@ namespace lab5
                 info = info + "\n" + line ;
                 string[] s = line.Split(' ');
                 int[] n = new int[4];
-                n[0] = int.Parse(s[3]);
-                n[1] = int.Parse(s[4]);
-                n[2] = int.Parse(s[5]);
-                n[3] = int.Parse(s[6]);
-                elevi.Add(new elev(s[0], s[1], int.Parse(s[2]), n));
+                n[0] = int.Parse(s[4]);
+                n[1] = int.Parse(s[5]);
+                n[2] = int.Parse(s[6]);
+                n[3] = int.Parse(s[7]);
+                elevi.Add(new elev(s[0] + " " + s[1], s[2], int.Parse(s[3]), n));
             }
             sr.Close();
         }
