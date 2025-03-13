d65f5c10bd511c89e56a4507d2b9af7e14aeb158
vasylec <cozma.vasyle@gmail.com>
2025-03-13 10:28:02 +0200

    lab teorie xml

---
+++ teorie (xml)/teorie (xml)/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace teorie__xml_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.import_button = new System.Windows.Forms.Button();
+            this.export_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // import_button
+            // 
+            this.import_button.Location = new System.Drawing.Point(12, 12);
+            this.import_button.Name = "import_button";
+            this.import_button.Size = new System.Drawing.Size(96, 31);
+            this.import_button.TabIndex = 0;
+            this.import_button.Text = "Import";
+            this.import_button.UseVisualStyleBackColor = true;
+            this.import_button.Click += new System.EventHandler(this.import_button_Click);
+            // 
+            // export_button
+            // 
+            this.export_button.Location = new System.Drawing.Point(113, 12);
+            this.export_button.Name = "export_button";
+            this.export_button.Size = new System.Drawing.Size(96, 31);
+            this.export_button.TabIndex = 1;
+            this.export_button.Text = "Export";
+            this.export_button.UseVisualStyleBackColor = true;
+            this.export_button.Click += new System.EventHandler(this.export_button_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 389);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.export_button);
+            this.Controls.Add(this.import_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button import_button;
+        private System.Windows.Forms.Button export_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ teorie (xml)/teorie (xml)/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teorie__xml_
+{
+    public partial class Form1 : Form
+    {
+        DataSet dataSet = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void import_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Title = "Select XML File";
+            o.Filter = "xml files (*.xml)|*.xml";
+            o.FilterIndex = 1;
+            
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                    dataSet.ReadXml(o.FileName);
+
+                    if (dataSet.Tables.Count > 0)
+                    {
+                        dataGridView1.DataSource = dataSet.Tables[0];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Fișierul XML nu conține tabele.", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("A apărut o eroare la citirea fișierului XML: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+           
+
+        }
+
+
+        private void export_button_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Title = "Save File";
+            s.Filter = "xml files (*.xml)|*.xml";
+
+
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                    dataSet.WriteXml(s.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("A apărut o eroare : " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
